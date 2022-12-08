235ef867b006a5ed4b9b2f2543b21c245fb76263
Trevor Lee <tlee297@my.bcit.ca>
2022-12-07 20:31:36 -0800

    integrate mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  markdown: {
+    drafts: true,
+  }, integrations: [react(), mdx({
+    drafts: true,
+  })]
 });
