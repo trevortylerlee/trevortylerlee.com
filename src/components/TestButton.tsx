5988b97b4643cc9782f42230bf9ae531e67b8a88
Trevor Lee <tlee297@my.bcit.ca>
2022-12-21 20:46:18 -0800

    copies to clipboard

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,13 +1,36 @@
 import Toastify from './Toastify';
 import toast from 'react-hot-toast';
 
-export default Toastify(function Button() {
+export default Toastify(function Button({message}) {
   const notify = () => {
-    toast.success('Here is the toast')
-    console.log('toasted')
+    if (message === 'trevortylerlee@gmail.com') {
+      try {
+        copy(message) && toast.success('Email copied to clipboard')
+      } catch (err) {
+        console.log(err)
+        toast.error('Error copying to clipboard')
+      }
+    } else {
+      try {
+        copy(message) && toast.success('Phone number copied to clipboard')
+      } catch (err) {
+        console.log(err)
+        toast.error('Error copying to clipboard')
+      }
+    }
+  }
+
+  const copy = async (message) => {
+    try {
+      navigator.clipboard.writeText(message)
+      return true
+    } catch (err) {
+      console.log(err)
+      return false
+    }
   }
 
   return <>
-    <button onClick={notify}>Notify</button>
+    <button onClick={notify}>{message}</button>
   </>
 })
