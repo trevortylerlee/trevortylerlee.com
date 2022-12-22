f3adc34436a39e473e63775432c7a9eef8e876d4
Trevor Lee <tlee297@my.bcit.ca>
2022-12-21 21:44:00 -0800

    does inline styling fix btn styling bug?

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -35,6 +35,17 @@ export default Toastify(function Button({message}) {
     <button 
       aria-label={message === 'trevortylerlee@gmail.com'
         ? 'Copy email' : 'Copy phone number'}
-      style={style} onClick={notify}>{message}</button>
+      onClick={notify}
+      style={{
+          borderRadius: '8px',
+          padding: '0.75rem',
+          fontWeight: 'bold',
+          background: '#ffffff',
+          border: 'none',
+          boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',
+          cursor: 'pointer',
+          transition: '0.2s'
+      }}
+      >{message}</button>
   </>
 })
