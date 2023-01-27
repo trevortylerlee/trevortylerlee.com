1234067da18c4e1592978c505f578ef2e16d71a3
Trevor Lee <tlee297@my.bcit.ca>
2023-01-26 19:37:48 -0800

    Revert "install vercell adapter"
    This reverts commit 758b9f9e1a62bd322aaf52a1ede53baee7536293.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from 'astro/config';
+
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   markdown: {
-    drafts: true
-  },
-  integrations: [react(), mdx({
-    drafts: true
-  })],
-  output: "server",
-  adapter: vercel()
+    drafts: true,
+  }, integrations: [react(), mdx({
+    drafts: true,
+  })]
 });
