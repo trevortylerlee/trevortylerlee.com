feff940b911951cdafd16cc727691a1f59524b3f
Trevor Lee <tlee297@my.bcit.ca>
2023-02-18 23:41:19 -0800

    add astro-robots-txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import robotsTxt from "astro-robots-txt";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://www.trevortylerlee.com',
   markdown: {
-    drafts: true,
-  }, integrations: [react(), mdx({
-    drafts: true,
-  })]
+    drafts: true
+  },
+  integrations: [react(), mdx({
+    drafts: true
+  }), robotsTxt()]
 });
