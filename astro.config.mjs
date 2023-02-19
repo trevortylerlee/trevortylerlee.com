8177600608971e90b8d22331ed2e97cf4053988f
Trevor Lee <tlee297@my.bcit.ca>
2023-02-18 19:55:36 -0800

    remove trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: 'https://www.trevortylerlee.com/',
+  site: 'https://www.trevortylerlee.com',
   markdown: {
     drafts: true,
   }, integrations: [react(), mdx({
