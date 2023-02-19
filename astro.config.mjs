a15836a4b345c5a772e7fed2fee73bd7560f3fd3
Trevor Lee <tlee297@my.bcit.ca>
2023-02-18 19:28:49 -0800

    add rss functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+  site: 'https://www.trevortylerlee.com/',
   markdown: {
     drafts: true,
   }, integrations: [react(), mdx({
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,22 @@
+import { getCollection, CollectionEntry } from 'astro:content';
+
+import rss from "@astrojs/rss";
+
+import { formatBlogPost } from '../js/utils';
+
+const postImportResult = await getCollection('posts')
+const posts = formatBlogPost(postImportResult)
+
+export const get = () =>
+  rss({
+    stylesheet: "/rss/styles.xsl",
+    title: "Trevor Tyler Lee's Website",
+    description: "Web developer from Vancouver, Canada 🇨🇦",
+    site: import.meta.env.SITE,
+    items: posts.map((post) => ({
+      link: `/blog/${post.slug}`,
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.description,
+    })),
+  });
