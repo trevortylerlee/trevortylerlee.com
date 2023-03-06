087f4b8813892159dc16ae4e025049cbd6598d30
Trevor Lee <tlee297@my.bcit.ca>
2023-03-06 14:05:49 -0800

    delete vercel json and vercel output

--- .vercel/output/static/_astro/ToastExample.85aae7a4.js
+++
@@ -1,1 +0,0 @@
-import{T as s,j as t,_ as r}from"./Toastify.e0872475.js";import"./index.45a47ed6.js";const l=s(function({message:o}){const n=()=>r("Hello World!");return t.jsx(t.Fragment,{children:t.jsx("button",{onClick:n,style:{},children:o})})});export{l as default};
