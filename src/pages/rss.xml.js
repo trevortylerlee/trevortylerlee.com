628d716c2e208bff6dac338b90fe848480dc1f16
Trevor Lee <tlee297@my.bcit.ca>
2023-03-15 12:12:20 -0700

    add rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,27 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { formatBlogPost } from "../js/utils";
+
+const postImportResult = await getCollection('posts');
+const posts = formatBlogPost(postImportResult);
+
+export function get(context) {
+  return rss({
+    // `<title>` field in output xml
+    title: "Trevor Lee's Website",
+    // `<description>` field in output xml
+    description: 'Digital designer and web developer in Vancouver, Canada',
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#contextsite
+    site: context.site,
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: posts.map((post) => ({
+      link: `/posts/${post.slug}`,
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.date,
+      customData: `<language>en-us</language>`,
+    })),
+  });
+}
