d06f0206aabfbe68be696c6394d74a7d06f2bf64
Trevor Lee <tlee297@my.bcit.ca>
2023-03-16 11:47:59 -0700

    name interactive, change theme toggle icons

--- src/components/Toastify.tsx
+++ src/components/Toastify.tsx
@@ -1,30 +1,30 @@
-import { Toaster } from "react-hot-toast";
+import { Toaster } from 'react-hot-toast';
 
 export default (Component) => {
   return (props) => (
     <>
       <Toaster
-        position="top-center"
+        position={props.position || 'top-center'}
         reverseOrder={false}
         gutter={8}
         containerClassName=""
         containerStyle={{}}
         toastOptions={{
           // Define default options
-          className: "",
-          duration: 2000,
+          className: '',
+          duration: 1000,
           style: {
-            background: "#363636",
-            color: "#fff",
+            background: '#333',
+            color: '#fff',
           },
 
           // Default options for specific types
           success: {
             duration: 2000,
             style: {
-              color: "black",
-              background: "white",
-              border: "3px solid black",
+              color: 'black',
+              background: 'white',
+              border: '3px solid black',
             },
           },
         }}
