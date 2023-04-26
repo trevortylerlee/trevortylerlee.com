7d2e8ab167ed59494b4918b343a12a74fc5924fa
Trevor Lee <tlee297@my.bcit.ca>
2023-04-25 22:35:24 -0700

    remove displayName line test

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -23,6 +23,6 @@ const TooltipContent = React.forwardRef<
     {...props}
   />
 ))
-TooltipContent.displayName = TooltipPrimitive.Content.displayName
+// TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
 export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
