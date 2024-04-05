f5f8510229b188613a35cc13d8c6264cd9cdf24c
Trevor Lee <49603972+trevortylerlee@users.noreply.github.com>
2024-04-05 00:12:53 -0700

    breadcrumbs hide last item on small screens. change post schema to limit title length.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,21 @@
 import { z, defineCollection } from "astro:content";
 
 const posts = defineCollection({
-  schema: ({image}) => z.object({
-    title: z.string().max(50, "Title must be 50 or fewer characters long."),
-    date: z.date(),
-    description: z
-      .string()
-      .max(
-        160,
-        "For best SEO results, keep the description under 160 characters."
-      )
-      .optional(),
-    draft: z.boolean().default(false),
-    imageSrc: image().optional(),
-    imageAlt: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().max(40, "Title must be 40 or fewer characters long."),
+      date: z.date(),
+      description: z
+        .string()
+        .max(
+          160,
+          "For best SEO results, keep the description under 160 characters.",
+        )
+        .optional(),
+      draft: z.boolean().default(false),
+      imageSrc: image().optional(),
+      imageAlt: z.string().optional(),
+    }),
 });
 
 export const collections = { posts };
