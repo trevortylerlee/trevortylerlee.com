8334c7aafadbdc57f692d58076dea9659dad7096
Trevor Lee <49603972+trevortylerlee@users.noreply.github.com>
2024-07-14 12:42:31 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://trevortylerlee.com",
+  site: "https://www.trevortylerlee.com",
   server: {
     port: 1999,
   },
